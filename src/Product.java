48f1dcd9f739910d65e9ebaa9c8c9d0f80aecdb9
nikos.skl <nikos.skl98@gmail.com>
2019-06-03 17:16:47 +0300

    Add auto increment id function for products

--- src/Product.java
+++ src/Product.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
 
 public class Product {
 
     private int id;
     private String name;
     private double price;
+    private static ArrayList<Product> products = new ArrayList<>();
 
-    public Product(int id, String name, double price) {
-        this.id = id;
+    public Product(String name, double price) {
+        this.id = generateId();
         this.name = name;
         this.price = price;
+        products.add(this);
     }
 
     public int getId() {
@@ -33,6 +36,24 @@ public class Product {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public static ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    private int generateId() {
+
+        // if there are no products
+        // give id: 1
+        if (products.size() == 0) {
+            return 1;
+        }
+        else {
+            // find last product's id
+            int lastId = products.get(products.size()-1).getId();
+            return lastId + 1;
+        }
     }
 
     @Override
